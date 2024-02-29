d0b66f9ba61ad2e54ca943f611afc3074f858acd
vapil <susulya00@mail.ru>
2024-02-29 12:12:33 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    abstract class Kerych
+    {
+        public abstract void katka();
+    }
+
+    class Vitun : Kerych
+    {
+        public override void katka()
+        {
+            Console.WriteLine("52NGG");
+        }
+    }
+    class Eexie : Kerych
+    {
+        public override void katka()
+        {
+            Console.WriteLine("58ЕК");
+        }
+    }
+    abstract class Sazuka : Kerych
+    {
+        public abstract override void katka();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp5;
+
+Vitun vitun = new Vitun();
+Eexie eexie = new Eexie();
+vitun.katka();
+eexie.katka();
